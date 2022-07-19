94e5c79c97961a13d8450b2034106500e17fd182
BySuspect <omerbayraktar43@gmail.com>
2022-07-19 14:20:41 +0300

    Add project files.

---
+++ CustomNumericUpDownSample.Android/CustomNumericEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using CustomNumericUpDownSample;
+using CustomNumericUpDownSample.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomNumericUpDownEntry), typeof(CustomNumericEntryRenderer))]
+namespace CustomNumericUpDownSample.Droid
+{
+    public class CustomNumericEntryRenderer : EntryRenderer
+    {
+        public CustomNumericEntryRenderer(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ CustomNumericUpDownSample.iOS/CustomNumericEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using CustomNumericUpDownSample;
+using CustomNumericUpDownSample.iOS;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomNumericUpDownEntry), typeof(CustomNumericEntryRenderer))]
+namespace CustomNumericUpDownSample.iOS
+{
+    public class CustomNumericEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
